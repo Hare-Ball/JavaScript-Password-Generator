9850d57de75c762fb64de62b09e12b21980eb02e
Hare-Ball <robby.hare@gmail.com>
2021-05-21 20:23:33 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,4 @@
+This password generator takes user input in the form of prompts, alerts and confirmations to generate a randomized password accoridng the criteria selected by the user. The user can select between upper case and lower case letters, numbers, and special characters for their password combination. The generator then randomly chooses between those selected items to create a random password of the desired length.
+<br>
+<br>
+<img src="Password_Generator_Screenshot.png" alt="Password Generator Screenshot">
--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// Create a function to generate the password
 function generatePassword() {
-    var passwordLength = prompt("Choose a number from 8 - 128"); 
+    var passwordLength = prompt("Choose a number from 8 - 128");
+    // Account for operator error 
     if (passwordLength < 8 || passwordLength > 128 ) {
       alert("Please enter a character length that is between 8 and 128.");
       return generatePassword();
   }
   if (passwordLength >=8 && passwordLength <= 128) {
+    // If entered correctly, continue confirming other possible variables as well.
     var upperCase = confirm("Do you want to include upper case letters?");
     var lowerCase = confirm("Do you want to include lower case letters?");
     var number = confirm("Do you want to include numbers?")
     var specialCharacter = confirm("Do you want to include special characters?");
   }
+  // Accont for operator error
   if (specialCharacter===false && number===false && lowerCase===false && upperCase===false) {
     alert("Please confirm at least one (*upper case, lower case, numbers, or special characters*).");
     return generatePassword();
@@ -43,6 +46,7 @@ function generatePassword() {
   var randomCharacter = upperCase + lowerCase + number + specialCharacter
 
   var password = " ";
+  //Create loop to continue generating random characters until the password length is achieved
     for (var i =0; i < passwordLength; i++) {
       var trueRandom = randomCharacter[Math.floor(Math.random() * randomCharacter.length)];
       password += trueRandom;
